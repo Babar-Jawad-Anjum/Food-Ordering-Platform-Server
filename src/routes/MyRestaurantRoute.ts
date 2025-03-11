db84b8226459deedda52cca42fb692ab061a3607
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2025-03-11 12:57:52 +0500

    feat: api validation added for restaurant routes

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
 import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyRestaurantRequest } from "../middlewares/validation";
 
 const router = express.Router();
 
@@ -15,9 +16,10 @@ const upload = multer({
 
 router.post(
   "/",
+  upload.single("imageFile"), // will embed file in req object which can be accessed by req.file
+  validateMyRestaurantRequest,
   jwtCheck,
   jwtParse,
-  upload.single("imageFile"), // will embed file in req object which can be accessed by req.file
   MyRestaurantController.createMyRestaurant
 );
 
